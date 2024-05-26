c068beabc3f4cfdef640a38085401237ccbcb0fa
Shion Noguchi <shion24stat@gmail.com>
2024-05-26 14:20:41 +0900

    Added initial setup of show command

--- morphy/main.py
+++ morphy/main.py
@@ -1,7 +1,8 @@
 import typer
 
-from .cmd import make, update
+from .cmd import make, update, show
 
 app = typer.Typer()
 app.add_typer(update.app, name="update")
 app.add_typer(make.app, name="make")
+app.add_typer(show.app, name="show")
