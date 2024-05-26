1636622f2b8ff5ef3e7c4a9fe4dda6bd27bf1e0f
Shion Noguchi <shion24stat@gmail.com>
2024-05-26 14:06:48 +0900

    Added implementation of make command

--- morphy/cmd/make.py
+++ morphy/cmd/make.py
@@ -1,9 +1,116 @@
+from typing import Annotated
+import os
+import datetime
+from pathlib import Path
 
 import typer
 from rich import print
+from rich.progress import track
+import pandas as pd
+
+from . import util
+from .. import config
 
 app = typer.Typer()
 
-@app.command('item')
-def make():
-    print('This is make item command. can you hear me?')
+
+def resample_to_Nsec(df: pd.DataFrame, interval: int) -> pd.DataFrame:
+    """
+    Resample the DataFrame to N-second data.
+
+    Args:
+        df(pd.DataFrame): DataFrame
+        interval(int): Interval in seconds
+    
+    Returns:
+        pd.DataFrame: Resampled DataFrame
+    
+    """
+
+    df["datetime"] = pd.to_datetime(df["datetime"])
+    df["open"] = df["open"].astype(float)
+    df["high"] = df["high"].astype(float)
+    df["low"] = df["low"].astype(float)
+    df["close"] = df["close"].astype(float)
+    df["volume"] = df["volume"].astype(float)
+    df["buyVolume"] = df["buyVolume"].astype(float)
+    df["sellVolume"] = df["sellVolume"].astype(float)
+
+    df = df.set_index("datetime")
+    df = df.resample(f"{interval}s").agg(
+        {
+            "open": "first",
+            "high": "max",
+            "low": "min",
+            "close": "last",
+            "volume": "sum",
+            "buyVolume": "sum",
+            "sellVolume": "sum",
+        }
+    )
+
+    df = df.dropna()
+
+    return df
+
+
+@app.command('item', help='Make N-second ohlcv data from original data in morphy storage.')
+@util.timer
+def make(
+    exchange: Annotated[str, typer.Argument(..., help="Exchange name")],
+    symbol: Annotated[str, typer.Argument(..., help="Symbol")],
+    begin: Annotated[str, typer.Argument(..., help="Begin date(YYYYMMDD)")],
+    end: Annotated[str, typer.Argument(..., help="End date(YYYYMMDD)")],
+    interval: Annotated[str, typer.Argument(..., help="Interval(sec)")],
+    to: Annotated[str, typer.Option(..., help="Savepath")] = '.',
+) -> None:
+    """
+    An implementation of the make item command of the Morphy CLI.
+    
+    """
+    
+    # <-- Input Validation -->
+    try:
+        fbegin = datetime.datetime.strptime(begin, "%Y%m%d")
+        fend = datetime.datetime.strptime(end, "%Y%m%d")
+    except ValueError:
+        err = f"Invalid date format. Use YYYYMMDD."
+        raise ValueError(err)
+
+    if fbegin > fend:
+        err = "Begin date should be earlier than end date."
+        raise ValueError(err)
+    
+    # posix path
+    to = Path(to)
+    if not to.exists():
+        raise ValueError(f"Path {to} does not exist.")
+    
+    file_name = f"{exchange}_{symbol}_{fbegin.strftime('%Y%m%d')}_{fend.strftime('%Y%m%d')}_{interval}s.csv.gz"
+    
+    # <-- Main Logic -->
+    date_range = pd.date_range(fbegin, fend, freq='D')
+    dfs = []
+    
+    for date in track(date_range, description='Making...'):
+
+        target = os.path.join(
+            config.STORAGE_DIR, exchange, symbol, f"{date.strftime('%Y%m%d')}.csv.gz"
+        )
+        if not os.path.exists(target):
+            # skip
+            continue
+
+        df = pd.read_csv(target, compression='gzip')
+        df = resample_to_Nsec(df, int(interval))
+        dfs.append(df)
+
+    if len(dfs) == 0:
+        print('No data to make. Use `morphy update` to download data.')
+        return
+    
+    final_df = pd.concat(dfs)
+    savepath = os.path.join(to, file_name)
+    final_df.to_csv(savepath, compression='gzip')
+
+    print("All processes are completed.")
